5630de4352db20c3e1064a1681e598d61c667cbd
Oscar <ogge_1998@hotmail.com>
2020-01-02 13:02:09 +0100

    Added range

--- interface.py
+++ interface.py
@@ -56,7 +56,7 @@ class PageOne(tk.Frame):
 
         f = Figure(figsize = (5, 5), dpi = 100)
         a = f.add_subplot(111)
-        a.plot([1,2,3,4,5,6,7,8], [5,1,2,3,6,6,7,8])
+        a.plot(range(1, 31), [5,1,2,3,6,6,7,8])
         canvas = FigureCanvasTkAgg(f, self)
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill = tk.BOTH, expand = True)
@@ -74,7 +74,7 @@ class PageTwo(tk.Frame):
 
         f = Figure(figsize = (5, 5), dpi = 100)
         a = f.add_subplot(111)
-        a.plot([1,2,3,4,5,6,7,8], [5,1,2,3,6,6,7,8])
+        a.plot(range(1, 31), [5,1,2,3,6,6,7,8])
         canvas = FigureCanvasTkAgg(f, self)
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill = tk.BOTH, expand = True)
@@ -95,7 +95,7 @@ class PageThree(tk.Frame):
 
         f = Figure(figsize = (5, 5), dpi = 100)
         a = f.add_subplot(111)
-        a.plot([1,2,3,4,5,6,7,8], [5,1,2,3,6,6,7,8])
+        a.plot(range(1, 31), [5,1,2,3,6,6,7,8])
         canvas = FigureCanvasTkAgg(f, self)
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill = tk.BOTH, expand = True)
